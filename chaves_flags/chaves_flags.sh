d1e268af4e9e8ce15a35d6f7c9497634f37b1cfc
Jesher Minelli <jesherdevsk8@gmail.com>
2021-11-26 21:08:25 -0300

    update chave_flags.sh

--- chaves_flags/chaves_flags.sh
+++ chaves_flags/chaves_flags.sh
@@ -20,8 +20,11 @@
 #	- Trocamos if pelo case       
 #	- Adicionado basename
 #   v1.2 23/11/2021, Jesher:
-#	- 
-#	-
+#	- Adicionado -m
+#	- Adicionado 2 flags
+#   v1.3 26/11/2021, Jesher:
+#	- Adicionado while com shift e teste de variável
+#	- Adicionado 2 flags
 # ------------------------------------------------------------------------ #
 # Testado em:
 #   versão 5.0.17
@@ -31,31 +34,34 @@ USUARIOS="$(cat /etc/passwd | cut -d : -f 1)"
 MENSAGEM_USO="
   $(basename $0) - [OPÇÕES]
   
-     Exemplos $0 -h & $0 -v 
+     Exemplos: $0 -h & $0 -v 
      	  & $0 -o
      
      -h - Menu de ajuda
      -v - Versão
      -s - Ordenar a saída
-     -m - Coloca em maiúsculo     
+     -m - Coloca em maiúsculo    
 "
-VERSAO="v1.2"
+VERSAO="v1.3"
 CHAVE_ORDENA=0
 CHAVE_MAIUSCULO=0
 # ------------------------------------------------------------------------ #
 
-
 # ------------------------------ Execução -------------------------------- #
+while test -n "$1"
+do
+   case "$1" in
+   -h) echo "$MENSAGEM_USO" && exit 0               ;;
+   -v) echo "$VERSAO" && exit 0                     ;;
+   -s) CHAVE_ORDENA=1				    ;;
+   -m) CHAVE_MAIUSCULO=1			    ;;
+    *) echo "Opção inválida, valie o -h" && exit 1  ;;
+   esac
+   shift
+done
 
-case "$1" in
-   -h) echo "$MENSAGEM_USO" && exit 0            ;;
-   -v) echo "$VERSAO" && exit 0                  ;;
-   -s) CHAVE_ORDENA=1				 ;;
-   -m) CHAVE_MAIUSCULO=1			 ;;
-    *) echo "$USUARIOS" | less   		 ;;
-esac
+[ $CHAVE_ORDENA -eq 1 ]    && USUARIOS=$(echo "$USUARIOS" | sort | less)
+[ $CHAVE_MAIUSCULO -eq 1 ] && USUARIOS=$(echo "$USUARIOS" | tr [a-z] [A-Z] | less)
 
-[ $CHAVE_ORDENA -eq 1 ] && echo "$USUARIOS" | sort | less
-[ $CHAVE_MAIUSCULO -eq 1 ] && echo "$USUARIOS" | tr [a-z] [A-Z] | less
-
+echo "$USUARIOS"
 # ------------------------------------------------------------------------ #
