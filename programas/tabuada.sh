418f110652df8abcac44c5bd282b096b67e3aad3
Jesher Minelli <jesherdevsk8@gmail.com>
2021-11-30 21:31:06 -0300

    update header

--- programas/tabuada.sh
+++ programas/tabuada.sh
@@ -1,13 +1,9 @@
 #! /usr/bin/env bash
 
-# tabuada.sh - É uma tabuada em Shell Script
-#
-# Repositório:  https://github.com/jesherdevsk8/Shell-Script/blob/master/programas/tabuada.sh
-# Autor:        Jesher Minelli
-#
-# Execução: 
-#	$ ./tabuada.sh
-#
+# tabuada.sh - É uma tabuada em Shell Script 
+# Autor: Jesher Minelli, https://github.com/jesherdevsk8/Shell-Script/blob/master/programas/tabuada.sh 
+# $ ./tabuada.sh | bash -x tabuada.sh (modo debug)
+# Data: 26/11/2021
 #---------------------- Título --------------------------
 echo "
 ---- Tabuada em shell script ----
@@ -19,7 +15,11 @@ read multiplica
 echo -e "\n\tTabuada do $multiplica\n"
 
 # ------------------- Execução --------------------------
+#Descomente set -x para debugar
+#set -x
 for i in $(seq 0 10)
 do
     resultado=$(($i*$multiplica)); echo -e "\t$i x $multiplica = $resultado"
 done
+#Descomente set +x para debugar
+#set +x
