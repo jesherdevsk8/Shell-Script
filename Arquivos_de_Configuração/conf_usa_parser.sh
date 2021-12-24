48534b06e78a839decc4e013bc5656e0c14b4e7c
Jesher Minelli <jesherdevsk8@gmail.com>
2021-12-23 23:56:41 -0300

    update

--- Arquivos_de_Configuração/conf_usa_parser.sh
+++ Arquivos_de_Configuração/conf_usa_parser.sh
@@ -14,6 +14,6 @@ eval $(./parser.sh $ARQUIVO_DE_CONFIGURACAO)
 [ "$(echo $CONF_USAR_CORES)" = "1" ] && USAR_CORES=1
 
 [ "$USAR_MAIUSCULAS" = "1" ] && MSG="$(echo $MSG | tr a-z A-Z)"
-[ "$USAR_CORES" = "1" ] && MSG="$(echo -E ${VERMELHO}$MSG)"
+[ "$USAR_CORES" = "1" ] && MSG="$(echo -e ${VERMELHO}$MSG)"
 
 echo -e "$MSG"
