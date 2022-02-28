5e859e70557a3599a7daeb365c45cacec41c65a4
Jesher Minelli <jesherdevsk8@gmail.com>
2022-02-28 18:31:55 -0300

    add condição para não aceitar números maiores que dez

--- programas/tabuada.sh
+++ programas/tabuada.sh
@@ -1,25 +1,25 @@
 #! /usr/bin/env bash
 
-# tabuada.sh - É uma tabuada em Shell Script 
+# tabuada.sh - É uma tabuada SIMPLES em Shell Bash 
 # Autor: Jesher Minelli, https://github.com/jesherdevsk8/Shell-Script/blob/master/programas/tabuada.sh 
-# $ ./tabuada.sh | bash -x tabuada.sh (modo debug)
+# $ ./tabuada.sh
 # Data: 26/11/2021
-#---------------------- Título --------------------------
+# Versão: 1.1
+#     Jesher Minelli - 28/02/2022: Adicionei condição para não aceitar números maiores que dez
+#---------------------- EXECUÇÃO --------------------------
 echo "
 ---- Tabuada em shell script ----
 "
-#-------------- Interação com usuário -------------------- 
+
     echo -n "Informe um número de 0 a 10:  "
 read multiplica
 
-echo -e "\n\tTabuada do $multiplica\n"
-
-# ------------------- Execução --------------------------
-#Descomente set -x para debugar
-#set -x
-for i in $(seq 0 10)
-do
+if [[ "$multiplica" -le 10 ]] ;then
+  echo -e "\n\tTabuada do $multiplica\n"
+    
+  for i in $(seq 0 10);do
     resultado=$(($i*$multiplica)); echo -e "\t$i x $multiplica = $resultado"
-done
-#Descomente set +x para debugar
-#set +x
+  done
+else
+  echo -e "Não válido.. Por favor digite um número menor ou igual a dez\nSaindo......."
+fi
