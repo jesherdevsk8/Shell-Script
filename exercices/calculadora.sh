8e7e6775ea7f4397762e67a1a04b5e5929c60573
Jesher Minelli <jesherdevsk8@gmail.com>
2022-02-28 18:34:14 -0300

    foi adicionado funções

--- exercices/calculadora.sh
+++ exercices/calculadora.sh
@@ -4,7 +4,6 @@
 #
 # site:		https://github.com/jesherdevsk8/Shell-Script/blob/master/exercices/calculadora.sh
 # Autor: 	Jesher Minelli Alves
-#
 # ----------------------------------------------------------------------------- #
 # Este programa irá auxilia-lo com calculos básicos
 #
@@ -15,9 +14,12 @@
 # Histórico
 #	v1.0 27/11/2021, Jesher:
 #	- Adicionado parâmetros -h, -m, -d, -s, -a
+#	v1.0.1 28/02/2022, Jesher:
+#	- Adicionado funções
 # ----------------------------------------------------------------------------- #
 # Testado em:
 #	Versão 5.0.17
+#
 # -------------------------------- VARIÁVEIS ---------------------------------- #
 msg="
   $(basename $0) - [OPÇÕES]
@@ -27,59 +29,68 @@ msg="
   	-d - dividir
   	-s - Subtração
   	-a - Adição
+  	-v - Versão
 "
+versao="v1.0.1"
 
-versao="v1.0"
+# ---------------------------------- FUNÇÕES ---------------------------------- #
+
+MULTIPLICAR(){
+  echo -n "Informe um número: " 
+  read num01
+  echo -n "Informe outro número: "
+  read num02
+  
+  result=$(($num01 * $num02))
+  echo "$num01 x $num02 = $result"
+}
+
+DIVIDIR(){
+  echo -n "Informe o dividendo: " 
+  read num01
+ 	echo -n "Informe o divisor: "
+ 	read num02
+ 	
+  if [[ $num01 -eq 0 ]]; then
+    echo "Não existe divisão por zero"
+  elif [[ $num02 -eq 0 ]]; then
+    echo "-- Não existe divisão por zero --"
+  else 
+    result=$(($num01 / $num02))
+ 	  echo "$num01 ÷ $num02 = $result"
+  fi
+}
+
+SUBTRACAO(){
+  echo -n "Informe um número: " 
+ 	read num01
+ 	echo -n "Informe outro número: "
+ 	read num02
+
+ 	result=$(($num01 - $num02))
+ 	echo "$num01 - $num02 = $result"
+}
+
+ADICAO(){
+ 	echo -n "Informe um número: " 
+ 	read num01
+ 	echo -n "Informe outro número: "
+ 	read num02
+    	
+ 	result=$(($num01 + $num02))
+  echo "$num01 + $num02 = $result"
+}
+
 # ---------------------------------- EXECUÇÃO --------------------------------- #
 while test -n "S1"
 do
   case "$1" in
-    -v) echo "$versao" && exit 0
-    	;;
-    
-    -h) 
-    	echo "$msg" && exit 0
-    	;;    	
-    -m) 
-    	echo "Informe um número: " 
-    	read num01
-    	echo "Informe outro número: "
-    	read num02
-    	result=$(($num01 * $num02))
-    	echo "$num01 x $num02 = $result" && exit 0
-    	;;
-    -d)
-    	echo "Informe o dividendo: " 
-    	read num01
-    	echo "Informe o divisor: "
-    	read num02
-    		if [[ $num01 -eq 0 ]]; then
-    		  echo "Não existe divisão por zero"
-    		elif [[ $num02 -eq 0 ]]; then
-    		  echo "-- Não existe divisão por zero --"
-    		else 
-    		  result=$(($num01 / $num02))
-    		  echo "$num01 ÷ $num02 = $result" && exit 0
-    		fi    	
-    	;;
-    -s)
-    	echo "Informe um número: " 
-    	read num01
-    	echo "Informe outro número: "
-    	read num02
-    	result=$(($num01 - $num02))
-    	echo "$num01 - $num02 = $result" && exit 0
-    	;;
-    -a)
-    	echo "Informe um número: " 
-    	read num01
-    	echo "Informe outro número: "
-    	read num02
-    	result=$(($num01 + $num02))
-    	echo "$num01 + $num02 = $result" && exit 0
-    	;;
-    	
-    *) echo "Opção inválida, valie o -h" && exit 1
-    	;;
+    -v) echo "$versao" && exit 0                     ;;    
+    -h) echo "$msg" && exit 0                        ;; # CHAMA FUNÇÃO MSG - modo de uso
+    -m) MULTIPLICAR && exit 0                        ;; # CHAMA FUNÇÃO MULTIPLICAR
+    -d) DIVIDIR && exit 0                            ;; # CHAMA FUNÇÃO DIVIDIR
+    -s) SUBTRACAO && exit 0                          ;; # CHAMA FUNÇÃO SUBTRAÇÃO
+    -a) ADICAO && exit 0                             ;; # CHAMA FUNCÃO ADICÃO   	
+     *) echo "Opção inválida, valie o -h" && exit 1  ;;
   esac
 done
