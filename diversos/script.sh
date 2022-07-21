254ef48562076f1f90f41b83bfa80d53935232a6
Jesher Minelli <jesheralves@claretiano.edu.br>
2022-07-21 16:02:56 -0300

    update comandos diversos

--- diversos/script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# deixa arquivo de texto de blob do banco em uma linha adicionando uma ',' virgula
-
-sed -i ':a;$!N;s/\n/,/;ta;' regex && cat regex
-
-# Trocar dono
-# sudo chown -R root:usuario /opt/apache-tomcat
-
